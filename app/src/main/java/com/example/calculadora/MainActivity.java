1fa4aee01050c8155b87f513dd6df30848c5c374
jperaltas <67621342+jperaltas@users.noreply.github.com>
2021-05-28 12:28:41 -0500

    Agregar variables e inicializaciones de las etiquetas en java

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText numero1,numero2;
+    TextView resultado;
+    Button boton;
+    Spinner operaciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numero1= findViewById(R.id.numero1);
+        numero2= findViewById(R.id.numero2);
+        resultado= findViewById(R.id.resultado);
+        boton=findViewById(R.id.boton);
+        operaciones=findViewById(R.id.spoperacion);
+
+        
+
+
+
+
     }
 }
